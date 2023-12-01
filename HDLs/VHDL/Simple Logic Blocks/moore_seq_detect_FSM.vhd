9c409616b3217c66dfedbe03913df654b09ff180
Supercobra27 <silverbergryan@gmail.com>
2023-11-30 23:46:00 -0500

    finished fsm i think

--- HDLs/VHDL/Simple Logic Blocks/moore_seq_detect_FSM.vhd
+++ HDLs/VHDL/Simple Logic Blocks/moore_seq_detect_FSM.vhd
@@ -18,6 +18,28 @@ begin
     begin
         if r = '0' then
             y <= A;
-        elsif
-
+        elsif (clock'EVENT and clock = '1') then
+          case y is
+            when A =>
+              if w = '0' then
+                y <= A;
+              else
+                y <= B;
+              end if;
+            when B =>
+              if w = '0' then
+                y <= A;
+              else
+                y  <= C;
+              end if;
+            when C =>
+              if w = '0' then
+                y <= A;
+              else
+                y <= C;
+              end if;
+            end case;
+          end if;
+        end process;
+    z <= '1' when y = C else '0';
 end arch ; -- arch
