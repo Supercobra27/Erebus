1697520624e6dc3fca7460b0fe880d95f01a6ff6
Supercobra27 <silverbergryan@gmail.com>
2023-12-27 23:46:16 -0500

    4

--- HDLs/VHDL/Simple Logic Blocks/ALU_tb.vhd
+++ HDLs/VHDL/Simple Logic Blocks/ALU_tb.vhd
@@ -17,7 +17,7 @@ architecture arch of ALU_tb is
         B : in std_logic_vector(7 downto 0);
         ALU_Sel : in  std_logic_vector(3 downto 0);
         ALU_Out : out  std_logic_vector(7 downto 0);
-        Carryout : out  std_logic;
+        Carryout : out  std_logic
        );
     end component;
 
